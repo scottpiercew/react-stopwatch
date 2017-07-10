72d54567a594507e1dabcad58fa980694c95674e
Scott Pierce <scottpiercew@gmail.com>
2017-07-10 15:30:25 -0400

    timer started

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -5,17 +5,30 @@ class Stopwatch extends Component {
   constructor (props) {
     super(props)
     this.state = {
+      seconds: 0,
+      paused: true,
+    }
+    this.timerId = null
+  }
 
+  handleStart(e) {
+    if(!this.timerId) {
+      this.timerId = setInterval( _ => this.updateTime(), 1000)
     }
   }
 
+  updateTime() {
+    this.setState({
+      seconds: this.state.seconds + 1,
+    })
+  }
   render() {
     return (
       <div className="stopwatch">
-        <h1>0</h1>
+        <h1>{this.state.seconds}</h1>
         <div className="controls">
           <button>Reset</button>
-          <button>Start</button>
+          <button onClick={ e => this.handleStart(e)}>Start</button>
           <button>Pause</button>
         </div>
       </div>
