e064443465bea61163ad758f311a6605a9054256
Scott Pierce <scottpiercew@gmail.com>
2017-07-10 14:57:16 -0400

    initial setup of a state

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import "./Stopwatch.css"
 
 class Stopwatch extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
   render() {
     return (
       <div className="stopwatch">
