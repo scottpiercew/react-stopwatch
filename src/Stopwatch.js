1558fd407141af03e372fe5e1d072ec1140dd07e
Scott Pierce <scottpiercew@gmail.com>
2017-07-11 10:25:25 -0400

    stopwatch complete

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -13,23 +13,50 @@ class Stopwatch extends Component {
 
   handleStart(e) {
     if(!this.timerId) {
-      this.timerId = setInterval( _ => this.updateTime(), 1000)
+      this.timerId = setInterval( start => this.updateTime(), 1000)
     }
   }
 
   updateTime() {
     this.setState({
       seconds: this.state.seconds + 1,
+      paused: false,
     })
   }
+
+  handlePause() {
+    let paused = !this.state.paused
+    if(paused) {
+      this.resetInterval()
+      this.setState({
+        paused,
+      })
+    } else {
+      this.handleStart()
+    }
+  }
+
+  resetInterval() {
+    clearInterval(this.timerId)
+    this.timerId = null
+  }
+
+  handleReset(e) {
+    this.resetInterval()
+    this.setState({
+      seconds: 0,
+      paused: true
+    })
+  }
+
   render() {
     return (
       <div className="stopwatch">
         <h1>{this.state.seconds}</h1>
         <div className="controls">
-          <button>Reset</button>
-          <button onClick={ e => this.handleStart(e)}>Start</button>
-          <button>Pause</button>
+          <button onClick={ e => this.handleReset(e) }>Reset</button>
+          <button onClick={ () => this.handleStart()}>Start</button>
+          <button onClick={ e => this.handlePause(e) }>Pause</button>
         </div>
       </div>
     );
